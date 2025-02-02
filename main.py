264d1e51424d1766c7eab84925f7b587b98672d6
Christopher Matheson <chrism24747@gmail.com>
2025-02-02 01:41:00 -0500

    both size at the same time and stats

--- main.py
+++ main.py
@@ -9,19 +9,19 @@ import threading
 FUNNYONE = False # Change to true to see the terrible 1 work (or not)
 lite = False #only uses one heuristic, for testing purposes
 
-puzzleSize = 4 # 3 or 4 supported, change for either 3x3 or 4x4 puzzle
+
 number_of_puzzles = 100 # Number of puzzles to solve
-max_threads = 1000 # Number of threads to use, if > number of puzzles, will use number of puzzles
+max_threads = 80 # Number of threads to use, if > number of puzzles, will use number of puzzles
 
 "Uses a lot of memory, if limited lower number of puzzles or threads <3"
 
-randomize = 5 # Number of random moves to make on the puzzle before solving
+randomize = 50 # Number of random moves to make on the puzzle before solving
 # more random moves = harder puzzle, = more ram, = bad idea
-def solve_puzzle(index,lock,results):
+def solve_puzzle(index,lock,results,size,stats):
     #Function to solve a puzzle and store results in a shared dictionary between threads.
-    puzzle = Puzzle(puzzleSize)
+    puzzle = Puzzle(size)
     puzzle.randomize(randomize)
-    solver = Solver(puzzle,puzzleSize)
+    solver = Solver(puzzle,size)
 
     output = f"puzzle {index + 1}\n{puzzle}"
 
@@ -35,7 +35,21 @@ def solve_puzzle(index,lock,results):
         solution, moves = solver.solve(heuristic)
         if solution:
             output += f"\nHeuristic:{heuristic}, Path Length: {moves}, Number of nodes explored: {solver.nodes_explored}"
-
+            #keeps track of stats per heuristic
+            if size == 4:
+                if heuristic+' For 4x4' in stats:
+                    with lock:
+                        stats[heuristic+' For 4x4'] += solver.nodes_explored
+                else:
+                    with lock:
+                        stats[heuristic+' For 4x4'] = solver.nodes_explored
+            else:
+                if heuristic+' For 3x3' in stats:
+                    with lock:
+                        stats[heuristic+' For 3x3'] += solver.nodes_explored
+                else:
+                    with lock:
+                        stats[heuristic+' For 3x3'] = solver.nodes_explored
     with lock:
         results[index] = output
 
@@ -50,23 +64,43 @@ if __name__ == '__main__':
 
 
     lock = multiprocessing.Lock()
+    stats = multiprocessing.Manager().dict({
+        'manhattan For 3x3': 0,
+        'manhattan For 4x4': 0,
 
+        'misplaced For 3x3': 0,
+        'misplaced For 4x4': 0,
+
+        'euclidian For 3x3': 0,
+        'euclidian For 4x4': 0
+    })
     x = 0
-    for i in range(number_of_puzzles):
-            while len(threads) >= max_threads:
-                for thread in threads:
-                    if not thread.is_alive():
-                        thread.join()
-                        threads.remove(thread)
-                        x += 1
-                        print(f"Thread finished {x/number_of_puzzles*100:.2f}%")
-            thread = multiprocessing.Process(target=solve_puzzle, args=(i, lock, results))
-            threads.append(thread)
-            thread.start()
+    for size in range(0,2):
+        for i in range(number_of_puzzles):
+                while len(threads) >= max_threads:
+                    for thread in threads:
+                        if not thread.is_alive():
+                            thread.join()
+                            threads.remove(thread)
+                            x += 1
+                            print(f"Thread finished {x/number_of_puzzles*50:.2f}%")
+                thread = multiprocessing.Process(target=solve_puzzle, args=(i +(number_of_puzzles*size), lock, results,size+3,stats))
+                threads.append(thread)
+                thread.start()
     for thread in threads:
         thread.join()
 
 
-    for i in range(number_of_puzzles):
-        print(results[i])
-        print("\n-----------------------------------------")
+
+    for i in range(number_of_puzzles*2):
+        if i in results:
+            print(results[i])
+            print("\n-----------------------------------------")
+    for i in range(number_of_puzzles * 2):
+        if i not in results:
+            print(f"puzzle {i+1} failed to solve")
+
+
+
+    for key in stats:
+        print(f"Average number of Nodes Explored for {key}: {stats[key] / number_of_puzzles}")
